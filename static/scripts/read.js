fd65123edfc1eeb0d72bd6b81d1cd44668ecf3a9
Aaron Lenoir <lenoir.aaron@gmail.com>
2017-03-13 01:35:16 +0100

    Adds first prototype of server-side reading with client-side paging.

---
+++ static/scripts/read.js
@@ -0,0 +1,35 @@
+// Read page code
+var read = (function (window, pager) {
+  var self = this;
+
+  self.bindEvents = function () {
+    var btnNext = window.document.getElementById('btnNextPage');
+    var btnPrevious = window.document.getElementById('btnPreviousPage');
+
+    btnNext.addEventListener('click', function (e) {
+      pager.nextPage();
+    });
+
+    btnPrevious.addEventListener('click', function (e) {
+      pager.previousPage();
+    });
+
+    window.document.addEventListener('keydown', function (e) {
+      if (e.which === 39) {
+        // Right arrow
+        pager.nextPage();
+      }
+
+      if (e.which === 37) {
+        // Left arraow
+        pager.previousPage();
+      }
+    });
+  };
+
+  self.init = function () {
+    self.bindEvents();
+  };
+
+  self.init();
+}(window, pager));
