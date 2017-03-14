338128c05acad64b9afe5f07ffda3ebd0e7d3a12
Aaron Lenoir <lenoir.aaron@gmail.com>
2017-03-14 01:16:38 +0100

    Adds 1 minute in-memory caching of article content.

--- app/article-reader.js
+++ app/article-reader.js
@@ -1,7 +1,27 @@
+const cache = require('memory-cache');
 const readability = require('node-readability');
 
+const timeToLiveMs = 60000;
+
 function read(uri, callback) {
-  readability(uri, callback);
+  var fromCache = cache.get(uri);
+  if (fromCache !== null) {
+    callback(undefined, fromCache.article, fromCache.meta);
+  } else {
+    readability(uri, function (err, article, meta) {
+      if (err) {
+        callback(err);
+      }
+
+      var data = {
+        article: article,
+        meta: meta
+      };
+      cache.put(uri, data, timeToLiveMs);
+      callback(err, article, meta);
+    });
+  }
 }
 
 module.exports = read;
+exports = read;
